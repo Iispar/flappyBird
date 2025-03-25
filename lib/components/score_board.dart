271aa3c32285303d4413ea89e7fbd7f9ef2dff24
Nelli Rossi <nnross@utu.fi>
2025-03-25 10:34:02 +0200

    score board visuals

--- lib/components/score_board.dart
+++ lib/components/score_board.dart
@@ -10,6 +10,44 @@ class ScoreBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => Text("${game.score}"));
+    return Stack(
+      children: [
+        Positioned(
+          top: MediaQuery.of(context).padding.top + 10,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Obx(() => Container(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlue[50],
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: const Color.fromARGB(255, 60, 117, 164),
+                        blurRadius: 5,
+                        offset: Offset(2, 2),
+                      ),
+                    ],
+                  ),
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "${game.score}",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 0, 129, 234),
+                            fontSize: 70,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
+          ),
+        ),
+      ],
+    );
   }
 }
